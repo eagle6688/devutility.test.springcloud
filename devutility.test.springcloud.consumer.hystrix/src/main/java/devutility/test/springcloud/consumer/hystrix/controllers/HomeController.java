ece6edd682e1eaba78f8869af39545a5ae9e5195
Aldwin Su <fangyansu@163.com>
2018-08-23 13:45:36 +0800

    Add hystrix

---
+++ devutility.test.springcloud.consumer.hystrix/src/main/java/devutility/test/springcloud/consumer/hystrix/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package devutility.test.springcloud.consumer.hystrix.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devutility.test.model.Person;
+
+import devutility.test.springcloud.consumer.hystrix.services.ProducerService;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+	@Autowired
+	private ProducerService producerService;
+
+	@RequestMapping("/hello/{name}")
+	public Person hello(@PathVariable("name") String name) {
+		return producerService.hello(name);
+	}
+
+	@RequestMapping("/getteacher")
+	public Person getTeacher() {
+		Person student = new Person();
+		student.setName("Lily");
+		return producerService.getTeacher(student);
+	}
+}
